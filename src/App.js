d3575df208f55bedddeeec3873a8d85b6b3bb6ea
Raj <rajrbanswal@gmail.com>
2022-01-27 20:14:53 +0530

    add all Changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Demo from './Demo';
+import Slider from './Slider';
+
+function App() {
+  return (
+    <div className="App">
+      <Demo />
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
